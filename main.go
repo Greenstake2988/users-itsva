96433533972303ea568e27b674f826b5a4cf175b
Oscar Vazquez <0skr.vazquez@gmail.com>
2023-08-09 15:53:22 -0600

    add cors allow all origin

--- main.go
+++ main.go
@@ -5,19 +5,23 @@ import (
 	"users-itsva/middlewares"
 	"users-itsva/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	//config.AllowAllOrigins = true
+	corsNoEstesChingando := cors.Default()
 	// Create db object
 	d := &database.DB{}
 	// Connect db object to Database
 	d.ConnectDB()
 
-	// Create router 
+	// Create router
 	router := gin.Default()
-
+	
+	router.Use(corsNoEstesChingando)
 	// Amake router use the function that Add Database to the context of gin
 	router.Use(middlewares.AddDBToContext(d.DB))
 
